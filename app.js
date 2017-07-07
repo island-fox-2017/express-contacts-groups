09e9300ad07464e8ed89ef4aad51b8e56201cd79
pdvega <pdvegaa@gmail.com>
2017-07-07 22:32:56 +0700

    release 1 contact

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+const app = express();
 const bodyParser = require('body-parser');
 
-
-var app = express();
+var sqlite = require('sqlite3').verbose();
+var db = new sqlite.Database('./db/contact_group.db');
 
 app.set('view engine', 'ejs');
 
@@ -11,16 +12,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-var arrResult = []
-
 app.get('/', function(req, res){
-  res.render('index', {data : arrResult})
+  res.send('Welcome aboard')
 })
 
-app.post('/', function(req, res){
-  let nama = req.body.name;
-  let address = req.body.address;
-  obj = {};
-  obj['name'] = nama;
-  obj['address'] = address;
+app.get('/contacts', function(req, res){
+  db.all('SELECT * FROM Contacts', function(err, rows){
+    res.render('contacts', {data_contact : rows})
+  })
 })
+
+app.post('/contacts', function(req, res){
+  db.run(`INSERT INTO Contacts(name, company, telp_number, email) VALUES ('${req.body.saveName}', '${req.body.saveCompany}', '${req.body.savePhone}', '${req.body.saveEmail}');`);
+  res.redirect('/contacts');
+})
+
+app.get('/contacts/delete/:id', function(req, res){
+  db.run(`DELETE FROM Contacts WHERE id=${req.params.id};`);
+  res.redirect('/contacts')
+})
+
+app.get('/contacts/edit/:id', function(req, res) {
+  db.all(`SELECT * FROM Contacts WHERE id=${req.params.id};`, function(err, rows){
+    res.render('editContacts',{data_contact : rows})
+  });
+})
+
+app.post('/contacts/edit/:id', function(req, res){
+  db.run(`UPDATE Contacts SET name='${req.body.saveName}',
+  company='${req.body.saveCompany}',
+  telp_number='${req.body.savePhone}',
+  email='${req.body.saveEmail}' WHERE
+  id='${req.params.id}'`)
+  res.redirect('/contacts')
+})
+
+
+
+
+
+app.listen(3000)
--- setup.js
+++ setup.js
@@ -3,15 +3,20 @@ var db = new sqlite.Database('./db/contact_group.db');
 
 function createTable(){
   db.run(`CREATE TABLE IF NOT EXISTS Contacts (
-    id INTEGER AUTO_INCREMENT, 
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
     name VARCHAR(50), 
     company VARCHAR(50),
     telp_number VARCHAR(15),
     email VARCHAR(50));`);
   
   db.run(`CREATE TABLE IF NOT EXISTS Groups (
-    id INTEGER AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name_of_group VARCHAR(50));`);
 };
 
-createTable();
+function insertData() {
+  db.run(`INSERT INTO Contacts(name, company, telp_number, email) VALUES ('teza', 'harsony', '081345678910', 'tezaharsony@gmail.com');`)
+}
+
+// createTable();
+// insertData()
